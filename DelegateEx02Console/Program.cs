6704efa970d93819fb92dee182ee7936b7d3f536
DavidLaughlin(WinVM) <davidclaughlin@gmail.com>
2017-04-17 12:08:55 -0700

    Exercises 0-1

--- DelegateEx02Console/Program.cs
+++ DelegateEx02Console/Program.cs
@@ -14,13 +14,12 @@ namespace DelegateEx02Console
             Console.WriteLine("Delegate as a Call-Back method");
 
             decimal[] prices = { 678.34m, 34.22m, 1293.11m };
-            Adjuster adj = new Adjuster(-25.0M);
-            AdjustDelegate adjustDelegate = adj.Apply;
+            
 
             // Second method argument is a call-back delegate,
             // which the method "plugs into" its code at the 
             // appropriate place(s).
-            DecimalTransformer.Transform(prices, adjustDelegate);
+            
 
             // Print adjusted prices
             foreach (var item in prices)
